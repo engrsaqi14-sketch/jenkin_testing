48e240e5008c0a8f5831449932070902c8092a46
saqib <engr.saqi14@gmail.com>
2026-01-26 17:47:34 +0100

    Set up basic Express server with a greeting route

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node app!');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
