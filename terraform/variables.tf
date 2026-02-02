a9bf592df9fc9441283b97fe3a9200c0a7451722
saqib <engr.saqi14@gmail.com>
2026-02-02 14:45:10 +0100

    Add Terraform variables for project configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+  variable "env" {
+  type = string
+}
+
+variable "region" {
+  description = "Region for project"
+  type        = string
+  default     = "me-south-1"
+}
+
+variable "ec2_ami" {
+  type = string
+}
+
+variable "ec2_family" {
+  type = string
+}
+
+variable "public_subnet" {
+  type = string
+}
+
+variable "WebAccess_sg" {
+  type =string
+}
+
+variable "RemoteAccess_sg" {
+  type = string
+}
+
+variable "myip_sg" {
+  type = string
+}
